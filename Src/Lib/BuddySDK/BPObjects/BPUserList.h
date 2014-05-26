2fc9851d515d450d63307bff726dd5ad99a73b79
Nick Ambrose <nick@buddy.com>
2014-05-26 12:31:15 -0700

    Comments for User List Fixed: createUser does not populate Gender_Unknown correctly.

--- Src/Lib/BuddySDK/BPObjects/BPUserList.h
+++ Src/Lib/BuddySDK/BPObjects/BPUserList.h
@@ -13,8 +13,8 @@
 @class BPUser;
 
 /**
-Enum for specifying UserListItemType.
-*/
+ * Enum for specifying UserListItemType.
+ */
 typedef NS_ENUM(NSInteger, BPUserListItemType)
 {
     /** User */
@@ -42,15 +42,55 @@ typedef NS_ENUM(NSInteger, BPUserListItemType)
 
 @interface BPUserList : BuddyObject<BPUserListProperties>
 
+/**
+ * Adds the id of the user to the user list.
+ *
+ * @param user        The users who's ID to add.
+ *
+ * @param callback    A callback to be called with a response.
+ *                    If error is nil then the result will be YES if the user ID was not already in the List.
+ *                    Or NO if the user ID was already present in the list.
+ *
+ */
 - (void)addUser:(BPUser *)user
        callback:(BuddyResultCallback)callback;
 
+/**
+ * Adds the id of the User to the UserList.
+ *
+ * @param user        The users who's ID to add.
+ *
+ * @param callback    A callback to be called with a response.
+ *                    If error is nil then the result will be YES if the user ID was not already in the List.
+ *                    Or NO if the user ID was already present in the list.
+ *
+ */
 - (void)addUserId:(NSString*)userId
        callback:(BuddyResultCallback)callback;
 
+/**
+ * Deletes the id of the User from the user list.
+ *
+ * @param user        The users who's ID to delete.
+ *
+ * @param callback    A callback to be called with a response.
+ *                    If error is nil then the result will be YES if the user ID was deleted from the List.
+ *                    Or NO if the user ID was not present in the list.
+ *
+ */
 - (void)deleteUser:(BPUser *)user
        callback:(BuddyResultCallback)callback;
 
+/**
+ * Deletes the id of the User from the user list.
+ *
+ * @param user        The users who's ID to delete.
+ *
+ * @param callback    A callback to be called with a response.
+ *                    If error is nil then the result will be YES if the user ID was deleted from the list.
+ *                    Or NO if the user ID was not present in the list.
+ *
+ */
 - (void)deleteUserId:(NSString*)userId
          callback:(BuddyResultCallback)callback;
 
