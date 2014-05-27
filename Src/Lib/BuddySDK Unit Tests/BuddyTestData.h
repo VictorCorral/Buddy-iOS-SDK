7322373fdc55f0f827f56d91f9b5192bba810b32
Nick Ambrose <nick@buddy.com>
2014-05-26 21:03:58 -0700

    Perpare for Release 2.0.4

--- Src/Lib/BuddySDK Unit Tests/BuddyTestData.h
+++ Src/Lib/BuddySDK Unit Tests/BuddyTestData.h
@@ -1,7 +1,7 @@
 
 
 #pragma message ("Insert your own AppID and Key")
-#define APP_NAME @"YOUR_APP_ID"
+#define APP_ID @"YOUR_APP_ID"
 #define APP_KEY @"YOUR_APP_KEY"
 
 #define TEST_USERNAME @"testuser"
