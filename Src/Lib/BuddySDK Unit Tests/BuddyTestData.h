2f2978d53a22b2a010cfbd369733ef03a78ae44b
bradserbuddy <brad@buddy.com>
2014-06-12 13:12:54 -0700

    Enable building under CI.

--- Src/Lib/BuddySDK Unit Tests/BuddyTestData.h
+++ Src/Lib/BuddySDK Unit Tests/BuddyTestData.h
@@ -1,8 +1,13 @@
 
 
 #pragma message ("Insert your own AppID and Key")
+#ifdef CI_APP_ID
+#define APP_ID CI_APP_ID
+#define APP_KEY CI_APP_KEY
+#else
 #define APP_ID @"YOUR_APP_ID"
 #define APP_KEY @"YOUR_APP_KEY"
+#endif
 
 #define TEST_USERNAME @"testuser"
 #define TEST_PASSWORD @"password"
