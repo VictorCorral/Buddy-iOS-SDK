e1852644cbd69314c74165da576ff52db9911339
Nick Ambrose <nick@buddy.com>
2014-08-12 09:05:35 -0700

    General cleanup

--- Samples/PhotoGallery/PhotoGallery/Constants.h
+++ Samples/PhotoGallery/PhotoGallery/Constants.h
@@ -12,9 +12,8 @@
 #define USERNAME_PREF_KEY @"BD_USERNAME"
 #define PASSWORD_PREF_KEY @"BD_PASSWORD"
 
-#pragma message ("Insert your own AppID and Key")
-#define APP_ID @"bbbbbc.PqhbvjvJgffh"
-#define APP_KEY @"46C5E9AE-332A-4EDE-9BE7-39B174F15DA6"
+#define APP_ID \@"Your App ID"
+#define APP_KEY \@"Your App Key"
 #define DEFAULT_BUT_CORNER_RAD 10
 #define DEFAULT_BUT_BORDER_WIDTH 1
 
